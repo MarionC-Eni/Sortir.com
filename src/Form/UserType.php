1fe71f1d6ad8fc1c9a0f535083dc90b47577c7b9
nicobdc44 <nbdcfr@hotmail.com>
2023-09-12 10:41:31 +0200

    test
    maj form register

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -6,20 +6,30 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+
 
 class UserType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('idUser')
-            ->add('pseudo')
-            ->add('name')
-            ->add('firstname')
-            ->add('phone')
-            ->add('email')
-            ->add('password')
-            ->add('photo')
+            ->add('idUser',integerType::class, ['label' => 'Prix du produit : '])
+            ->add('pseudo', TextType::class, ['label' => 'Pseudo : '])
+            ->add('name', TextType::class, ['label' => 'Nom : '])
+            ->add('firstname', TextType::class, ['label' => 'Prénom : '])
+            ->add('phone', IntegerType::class, ['label' => 'Téléphone : '])
+            ->add('email', EmailType::class, ['label' => 'Email : '])
+            ->add('password', PasswordType::class, ['label' => 'Mot de passe : '])
+            ->add('photo', FileType::class, ['label' => 'Photo : '])
         ;
     }
 
