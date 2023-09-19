b4e562b34d36cebee03ccd9ec5c76ea231a66489
MarionC-Eni <mcraheix@gmail.com>
2023-09-19 17:51:07 +0200

    création d'un service pour gérer les états. uniquement fonction publish realise. + mise à jour de tous les fichiers correspondants

---
+++ src/Controller/SubscriptionController.php
@@ -0,0 +1,122 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Event;
+use App\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+
+
+#[Route('/event')]
+class SubscriptionController extends AbstractController
+{
+//    #[Route('/subscription', name: 'app_subscription')]
+//    public function index(): Response
+//    {
+//        return $this->render('subscription/index.html.twig', [
+//            'controller_name' => 'SubscriptionController',
+//        ]);
+//    }
+//}
+    #[Route('/subscription/{eventId}', name: 'app_subscription')]
+    public function subscribeToEvent($eventId, EntityManagerInterface $entityManager)
+    {
+
+        // je recherche l'événement auquel je veux m'inscrire :
+        $event = $entityManager->getRepository(Event::class)->find($eventId);;
+
+        // pour le moment, je crée une exception très simple
+        if (!$event) {
+            throw $this->createNotFoundException('Événement non trouvé');
+        }
+
+        // je suis bien connectée à mon site, sinon l'inscription est impossible :
+        $user = $this->getUser();
+
+        // pour le moment, je crée une exception très simple. on vérifiera plus tard si l'utilsiateur est déjà inscrit
+        if (!$user) {
+            throw $this->createNotFoundException('Utilisateur non connecté');
+        }
+
+        // WARNING : il manque PLEIN de conditions !!!
+
+        // j'obtiens l'id de l'utilisateur connecté qui va s'inscrire à l'événement:
+//        $userId = $user->getId();
+
+//       ce code marche uniquement si on crée une nouvelle entité EventSubscription
+// // je crée la nouvelle inscription à cet événement
+//        $eventSubscription = new EventSubscription();
+//        $eventSubscription->setUser($user);
+//        $eventSubscription->setEvent($event);
+
+        $user->addRegistredevent($event);
+
+
+        // j'envoie mon inscription à ma base de données
+//        $em->persist($eventSubscription);
+        $entityManager->flush();
+
+//        //on refresh la sortie pour avoir le bon nombre d'inscrits avant le tchèque ci-dessous
+//        $em->refresh($event);
+
+        $this->addFlash("success", "Vous êtes inscrit/es !");
+
+        return $this->redirectToRoute('app_event_index', [], Response::HTTP_SEE_OTHER);
+
+//        return $this->render('subscription/index.html.twig', [
+//            'controller_name' => 'SubscriptionController',
+//        ]);
+    }
+
+    #[Route('/unsubscription/{eventId}', name: 'app_unsubscription')]
+    public function unsubscribeToEvent($eventId, EntityManagerInterface $entityManager)
+    {
+        // je recherche l'événement auquel je veux m'inscrire :
+        $event = $entityManager->getRepository(Event::class)->find($eventId);;
+
+        // pour le moment, je crée une exception très simple
+        if (!$event) {
+            throw $this->createNotFoundException('Événement non trouvé');
+        }
+
+// je suis bien connectée à mon site, sinon l'inscription est impossible :
+        $user = $this->getUser();
+
+// pour le moment, je crée une exception très simple. on vérifiera plus tard si l'utilsiateur est déjà inscrit
+        if (!$user) {
+            throw $this->createNotFoundException('Utilisateur non connecté');
+        }
+
+// j'obtiens l'id de l'utilisateur connecté qui va s'inscrire à l'événement:
+//        $userId = $user->getId();
+
+//       ce code marche uniquement si on crée une nouvelle entité EventSubscription
+// // je crée la nouvelle inscription à cet événement
+//        $eventSubscription = new EventSubscription();
+//        $eventSubscription->setUser($user);
+//        $eventSubscription->setEvent($event);
+
+        $user->removeRegistredevent($event);
+
+
+// j'envoie mon inscription à ma base de données
+//        $em->persist($eventSubscription);
+        $entityManager->flush();
+
+//        //on refresh la sortie pour avoir le bon nombre d'inscrits avant le tchèque ci-dessous
+//        $em->refresh($event);
+
+        $this->addFlash("success", "Vous êtes désinscrit/es !");
+
+        return $this->redirectToRoute('app_event_index', [], Response::HTTP_SEE_OTHER);
+
+//        return $this->render('subscription/index.html.twig', [
+//            'controller_name' => 'SubscriptionController',
+//        ]);
+
+    }
+}
